ff637c9896fecd357275d607c8b23e92c6790148
quruihang <quruihang.happy@163.com>
2019-03-13 16:34:47 +0800

    添加验证码

---
+++ config/captcha.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * characters 选项是用来显示给用户的所有字符串
+ * default, flat, mini, inverse 分别是定义的四种验证码类型
+ * 可以在此修改对应选项自定义验证码的长度、背景颜色、文字颜色等属性
+ */
+
+return [
+
+    'characters' => '2346789abcdefghjmnpqrtuxyzABCDEFGHJMNPQRTUXYZ',
+
+    'default'   => [
+        'length'    => 9,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+        'math'      => true,
+    ],
+
+    'flat'   => [
+        'length'    => 6,
+        'width'     => 160,
+        'height'    => 46,
+        'quality'   => 90,
+        'lines'     => 6,
+        'bgImage'   => false,
+        'bgColor'   => '#ecf2f4',
+        'fontColors'=> ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
+        'contrast'  => -5,
+    ],
+
+    'mini'   => [
+        'length'    => 3,
+        'width'     => 60,
+        'height'    => 32,
+    ],
+
+    'inverse'   => [
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+        'sensitive' => true,
+        'angle'     => 12,
+        'sharpen'   => 10,
+        'blur'      => 2,
+        'invert'    => true,
+        'contrast'  => -5,
+    ]
+
+];
