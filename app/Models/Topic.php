01ec16ea1e9bc95fc129c0476bdbeb5498a668d9
quruihang <quruihang.happy@163.com>
2019-03-14 22:22:58 +0800

    优化topics样式
    优化topics样式

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,17 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 新增 category 和 user 的模型关联
+    public function category()
+    {
+        // 一个话题属于一个分类；
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        // 一个话题拥有一个作者。
+        return $this->belongsTo(User::class);
+    }
 }
