506a14975ca7c8001beec148cd6ceebd8f64f37b
quruihang <quruihang.happy@163.com>
2019-03-15 11:43:24 +0800

    使用 HTMLPurifier for Laravel 5 对话题内容进过滤
    使用 HTMLPurifier for Laravel 5 对话题内容进行过滤

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,6 +22,8 @@ class TopicObserver
     // 在 Topic 模型保存时触发的 saving 事件中，对 excerpt 字段进行赋值
     public function saving(Topic $topic)
     {
+        // 在数据入库前进行过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
         // make_excerpt() 是自定义的辅助方法，需要在 helpers.php 文件中添加
         $topic->excerpt = make_excerpt($topic->body);
     }
